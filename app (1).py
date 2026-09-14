9ad63e9d0493da2e93b952ca4009ce5f0902c9db
Nehaa215 <nehakadam51830@gmail.com>
2026-09-14 19:34:43 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,22 @@
+
+import dash
+from dash import dcc, html, Input, Output
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv("/content/final_dataset.csv")
+
+app = dash.Dash(__name__)
+
+server = app.server
+
+# PUT YOUR app.layout CODE HERE
+
+# PUT YOUR @app.callback CODE HERE
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8050,
+        debug=False
+    )
