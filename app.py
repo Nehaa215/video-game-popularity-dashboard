9d625fa2e782c2421298ca3cc62e3e8ab465da73
Nehaa215 <nehakadam51830@gmail.com>
2026-09-14 20:22:32 +0530

    app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ import pandas as pd
 df = pd.read_csv("final_dataset.csv")
 
 app = dash.Dash(__name__)
+app.layout = html.Div([
+    html.H1("Video Game Popularity Dashboard"),
+    html.P("Dashboard is working!")
+])
 
 server = app.server
 
